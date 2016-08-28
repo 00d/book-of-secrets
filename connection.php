51461c6d0627798ff5a0299f35187036d8641acc
Johnny B <ziggynose@gmail.com>
2016-08-28 15:11:49 -0700

    Initial
    Moving project from drive to git

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+    $link = mysqli_connect("localhost", "bookofsecrets", "D-fnT^Hbz", "bookofsecrets");
+        
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
+            
+        }
+
+?>
